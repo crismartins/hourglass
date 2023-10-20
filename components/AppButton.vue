7c5177d0a045c14275927af05663cb52d77b6330
Cristopher <cristophermartins.77@gmail.com>
2023-10-20 19:09:05 -0300

    💄 add more button styles

--- components/AppButton.vue
+++ components/AppButton.vue
@@ -22,7 +22,7 @@ const props = defineProps({
 const { hasLink } = toRefs(props)
 </script>
 
-<style lang="scss">
+<style lang="scss" >
 .button{
     background-color: var(--lighter_grey);
     color: var(--blue);
@@ -41,6 +41,56 @@ const { hasLink } = toRefs(props)
     @media(max-width:$br_mobile){
         font-size: $size_20px;
     }
+    &.primary{
+        background-color: var(--blue);
+        color: var(--pure_white);
+        &:hover{
+            background-color: var(--dark_blue);
+        }
+    }
+    &.register{
+        background-color: var(--green);
+        color: var(--pure_white);
+        gap: 12px;
+        &:hover{
+            color: var(--darker_color);
+        }
+        &:before{
+            content: '';
+            display: block;
+            width: 12px;
+            aspect-ratio: 1;
+            background-color: var(--pure_white);
+            border-radius: 50%;
+            box-shadow: 0 0 0 4px;
+            color: var(--lighter_grey);
+            transition: $transition_default;
+        }
+        i{
+            font-size: $size_24px;
+        }
+        &.active{
+            background-color: var(--pure_white);
+            color: var(--grey);
+            &:before{
+                background-color: var(--green);
+                animation: 2s pulseShadows ease infinite;
+                color: var(--light_green);
+            }
+            i{
+                rotate: 180deg;
+                animation: 2s takeBreak ease;
+            }
+        }
+    }
+    &.full-width{
+        width: 100%;
+    }
+    &.icon{
+        i{
+            font-size: $size_20px;
+        }
+    }
     &.small{
         font-size: $size_14px;
     }
@@ -48,6 +98,30 @@ const { hasLink } = toRefs(props)
         padding: 0;
         background-color: transparent;
         color: var(--light_grey);
+    }
+}
+
+@keyframes pulseShadows{
+    0%{
+        box-shadow: 0 0 0 4px;
+    }
+    50%{
+        box-shadow: 0 0 0 8px;
+    }
+    100%{
+        box-shadow: 0 0 0 4px;
+    }
+}
+@keyframes takeBreak{
+    
+    20%{
+        transform: rotate(180deg);
+    }
+    90%{
+        transform: translateX(-120px) rotate(180deg);
+    }
+    100%{
+        opacity: 0;
     }
 }
 </style>
